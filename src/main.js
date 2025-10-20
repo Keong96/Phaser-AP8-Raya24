b2a88cd3ec7086ac990602188410eec5cb3d51cc
Jack <jack@gitlab.com>
2025-10-20 11:51:42 +0800

    chore: optimize build with chunk splitting

--- src/main.js
+++ src/main.js
@@ -1,22 +1,34 @@
-import Phaser from 'phaser'
-import confetti from 'canvas-confetti'
+const bootstrap = async () => {
+	const [
+		{ default: Phaser },
+		{ default: confetti },
+		{ default: GameScene },
+	] = await Promise.all([
+		import('phaser'),
+		import('canvas-confetti'),
+		import('./GameScene'),
+	])
 
-import GameScene from './GameScene'
+	// expose frequently used libraries to the game globals
+	window.Phaser = Phaser
+	window.confetti = confetti
 
-// Make confetti available globally for the game
-window.confetti = confetti
+	const config = {
+		type: Phaser.AUTO,
+		parent: 'game-container',
+		scale: {
+			mode: Phaser.Scale.ENVELOP,
+			autoCenter: Phaser.Scale.CENTER_BOTH,
+			width: 1080,
+			height: 1920,
+			orientation: Phaser.Scale.Orientation.PORTRAIT,
+		},
+		scene: [GameScene],
+	}
 
-const config = {
-	type: Phaser.AUTO,
-	parent: 'game-container',
-	scale: {
-		mode: Phaser.Scale.ENVELOP,
-		autoCenter: Phaser.Scale.CENTER_BOTH,
-		width: 1080,
-		height: 1920,
-		orientation: Phaser.Scale.Orientation.PORTRAIT,
-	},
-	scene: [GameScene],
+	return new Phaser.Game(config)
 }
 
-export default new Phaser.Game(config)
+bootstrap().catch((err) => {
+	console.error('Failed to bootstrap the game', err)
+})
